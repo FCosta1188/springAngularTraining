d722956797b3dee42a9e50dd9deb686bcee87c98
FCosta1188 <55804246+FCosta1188@users.noreply.github.com>
2024-03-01 15:26:34 +0100

    Added command line runner (init) and application.yml

--- server/src/main/groovy/dev/fcosta/server/ServerApplication.groovy
+++ server/src/main/groovy/dev/fcosta/server/ServerApplication.groovy
@@ -1,7 +1,12 @@
 package dev.fcosta.server
 
+import dev.fcosta.server.enumeration.Status
+import dev.fcosta.server.model.Server
+import dev.fcosta.server.repo.ServerRepo
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
 
 //***** https://www.youtube.com/watch?v=8ZPsZBcue50 *****
 /**
@@ -14,6 +19,55 @@ class ServerApplication {
 
     static void main(String[] args) {
         SpringApplication.run(ServerApplication, args)
+    }
+
+    @Bean
+    CommandLineRunner init(ServerRepo serverRepo) {
+        return {
+            serverRepo.save(new Server(
+                    null,
+                    "192.168.1.160",
+                    "Ubuntu Linux",
+                    "16 GB",
+                    "Personal PC",
+                    "http://localhost:8080/server/image/server1.png",
+                    Status.SERVER_UP
+            )
+            )
+
+            serverRepo.save(new Server(
+                    null,
+                    "192.168.1.160",
+                    "Ubuntu Linux",
+                    "16 GB",
+                    "Personal PC",
+                    "http://localhost:8080/server/image/server2.png",
+                    Status.SERVER_UP
+            )
+            )
+
+            serverRepo.save(new Server(
+                    null,
+                    "192.168.1.160",
+                    "Ubuntu Linux",
+                    "16 GB",
+                    "Personal PC",
+                    "http://localhost:8080/server/image/server3.png",
+                    Status.SERVER_UP
+            )
+            )
+
+            serverRepo.save(new Server(
+                    null,
+                    "192.168.1.160",
+                    "Ubuntu Linux",
+                    "16 GB",
+                    "Personal PC",
+                    "http://localhost:8080/server/image/server4.png",
+                    Status.SERVER_UP
+            )
+            )
+        }
     }
 
 }
