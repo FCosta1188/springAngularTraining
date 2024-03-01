bd41e2a7546d989b8a2ee71e5a931b9bbca785cb
FCosta1188 <55804246+FCosta1188@users.noreply.github.com>
2024-03-01 14:52:30 +0100

    Added ServerResource

--- server/src/main/groovy/dev/fcosta/server/model/Response.groovy
+++ server/src/main/groovy/dev/fcosta/server/model/Response.groovy
@@ -1,4 +1,22 @@
 package dev.fcosta.server.model
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import groovy.transform.Canonical
+import groovy.transform.builder.Builder
+import lombok.experimental.SuperBuilder
+import org.springframework.http.HttpStatus
+
+import java.time.LocalDateTime
+
+@Canonical
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL) // don't include null values in Response json
 class Response {
+    protected LocalDateTime timestamp
+    protected int statusCode
+    protected HttpStatus status
+    protected String reason
+    protected String message
+    protected String developerMessage
+    protected Map<?, ?> data
 }
---
+++ server/src/main/groovy/dev/fcosta/server/resource/ServerResource.groovy
@@ -0,0 +1,101 @@
+package dev.fcosta.server.resource
+
+import dev.fcosta.server.enumeration.Status
+import dev.fcosta.server.model.Response
+import dev.fcosta.server.model.Server
+import dev.fcosta.server.service.implementation.ServerServiceImpl
+import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+import javax.print.attribute.standard.Media
+import java.awt.PageAttributes
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.time.LocalDateTime
+
+@RestController
+@RequestMapping("/server")
+class ServerResource {
+    final ServerServiceImpl serverService
+
+    @GetMapping("/list")
+    ResponseEntity<Response> getServers() {
+        return ResponseEntity<Response>.ok(
+                Response.builder()
+                        .timestamp(LocalDateTime.now())
+                        .data(Map.of("servers", serverService.list(30)))
+                        .message("Servers retrieved")
+                        .status(HttpStatus.OK)
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        )
+    }
+
+    @GetMapping("/ping/{ipAddress}")
+    ResponseEntity<Response> pingServer(@PathVariable("ipAddress") String ipAddress) {
+        Server server = serverService.ping(ipAddress)
+
+        return ResponseEntity<Response>.ok(
+                Response.builder()
+                        .timestamp(LocalDateTime.now())
+                        .data(Map.of("server", server))
+                        .message(server.status == Status.SERVER_UP ? "Ping success" : "Ping failed")
+                        .status(HttpStatus.OK)
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        )
+    }
+
+    @PostMapping("/save")
+    ResponseEntity<Response> saveServer(@RequestBody @Valid Server server) {
+        return ResponseEntity<Response>.ok(
+                Response.builder()
+                        .timestamp(LocalDateTime.now())
+                        .data(Map.of("server", serverService.create(server)))
+                        .message("Server created")
+                        .status(HttpStatus.CREATED)
+                        .statusCode(HttpStatus.CREATED.value())
+                        .build()
+        )
+    }
+
+    @GetMapping("/get/{id}")
+    ResponseEntity<Response> getServer(@PathVariable("id") Long id) {
+        return ResponseEntity<Response>.ok(
+                Response.builder()
+                        .timestamp(LocalDateTime.now())
+                        .data(Map.of("server", serverService.get(id)))
+                        .message("Server retrieved")
+                        .status(HttpStatus.OK)
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        )
+    }
+
+    @DeleteMapping("/delete/{id}")
+    ResponseEntity<Response> deleteServer(@PathVariable("id") Long id) {
+        return ResponseEntity<Response>.ok(
+                Response.builder()
+                        .timestamp(LocalDateTime.now())
+                        .data(Map.of("deleted", serverService.delete(id)))
+                        .message("Server deleted")
+                        .status(HttpStatus.OK)
+                        .statusCode(HttpStatus.OK.value())
+                        .build()
+        )
+    }
+
+    @GetMapping(path = "/image/{fileName}", produces = MediaType.IMAGE_JPEG_VALUE)
+    byte[] getServerImage(@PathVariable("fileName") String fileName) {
+        return Files.readAllBytes(Paths.get(System.getProperty("user.home") + "Downloads/images/" + fileName))
+    }
+}
